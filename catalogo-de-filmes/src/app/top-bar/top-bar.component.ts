cf439e4de38b47ce5031268d08f48ab6a72ae223
Iana Mary <ianamary@alu.ufc.br>
2020-04-09 18:05:14 -0300

    adicionado as funções Aumentar, diminuir e restaurar a fonte | ajuste na tela de listar filmes

--- catalogo-de-filmes/src/app/top-bar/top-bar.component.ts
+++ catalogo-de-filmes/src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-top-bar',
@@ -6,22 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent implements OnInit {
+  
 
   constructor(
-    
-  ) {
-    var alto = document.getElementById("alto-contraste");
-    alto.classList.toggle('alto-contraste');
-  }
-
-  ngOnInit(
-   
   ) {}
 
-  muda() {
-    var alto = document.getElementById("alto-contraste");
-    alto.classList.toggle('alto-contraste');
+  ngOnInit(
+
+  ) { this.altoContraste(); }
+
+  altoContraste() {
+    document.getElementById("acessibilidade").classList.toggle('alto-contraste');
   }
- 
+
+  fonte (number: Number) {
+    var tag = document.getElementsByTagName("body")[0];
+    let fonteString = window.getComputedStyle(tag, null).getPropertyValue('font-size');
+    let fontNumber = parseFloat(fonteString);
+     //AUMENTAR A FONTE 
+    if( (number == 1) && (fontNumber < 25)) {
+      fontNumber++;
+      tag.style.fontSize = fontNumber + "px"
+      console.log( tag.style.fontSize);
+    }
+
+    //REDEFINIR A FONTE
+    else if(number == 0) {
+      fontNumber++;
+      tag.style.fontSize = "16px";
+      console.log( tag.style.fontSize);
+    }
+
+     //DIMINUIR A FONTE 
+     else if( (number == -1) && (fontNumber > 12)) {
+      fontNumber--;
+      tag.style.fontSize = fontNumber + "px"
+      console.log( tag.style.fontSize);
+    }
+   
+  }
 
 }
